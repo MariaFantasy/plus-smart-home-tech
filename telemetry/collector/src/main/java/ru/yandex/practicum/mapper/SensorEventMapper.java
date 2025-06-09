088ff7865503f17475b2ede831534dc38051dcfa
MariaFantasy <peggy-sju@ya.ru>
2025-06-09 19:58:11 +0300

    Корректировка маппера

--- telemetry/collector/src/main/java/ru/yandex/practicum/mapper/SensorEventMapper.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/mapper/SensorEventMapper.java
@@ -1,10 +1,11 @@
 package ru.yandex.practicum.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.yandex.practicum.dto.sensor.SensorEvent;
 import ru.yandex.practicum.kafka.telemetry.event.*;
 
 @Mapper(componentModel = "spring")
-public class SensorEventMapper {
+public interface SensorEventMapper {
     SensorEventAvro mapToAvro(SensorEvent event);
 }
