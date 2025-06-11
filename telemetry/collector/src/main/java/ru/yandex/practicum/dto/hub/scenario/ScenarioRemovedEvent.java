b9413497c4c61e171005a649d00df69a1bee7e7f
MariaFantasy <peggy-sju@ya.ru>
2025-06-12 00:48:40 +0300

    Доработка pom.xml

--- telemetry/collector/src/main/java/ru/yandex/practicum/dto/hub/scenario/ScenarioRemovedEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/dto/hub/scenario/ScenarioRemovedEvent.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.dto.hub.scenario;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import ru.yandex.practicum.dto.hub.HubEvent;
 import ru.yandex.practicum.dto.hub.HubEventType;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class ScenarioRemovedEvent extends HubEvent {
     private String name;
 
