6bba1132d51b13d55d3a96bad864d204a94b4efe
Maria <peggy-sju@ya.ru>
2025-06-15 19:52:15 +0300

    Поиск ошибки

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/CollectorServiceImpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/CollectorServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.KafkaProperties;
 import ru.yandex.practicum.dto.hub.HubEvent;
@@ -8,6 +9,7 @@ import ru.yandex.practicum.dto.sensor.SensorEvent;
 import ru.yandex.practicum.mapper.HubEventMapper;
 import ru.yandex.practicum.mapper.SensorEventMapper;
 
+@Slf4j
 @Service("collectServiceImpl")
 @RequiredArgsConstructor
 public class CollectorServiceImpl implements CollectorService {
@@ -17,10 +19,14 @@ public class CollectorServiceImpl implements CollectorService {
     private final KafkaProperties kafkaProperties;
 
     public void loadSensorEvent(SensorEvent sensorEvent) {
+        log.info("Start sensor event save {}", sensorEvent);
         kafkaProducer.send(sensorEventMapper.mapToAvro(sensorEvent), kafkaProperties.getTopic().getSensor());
+        log.info("Success sensor event save {}", sensorEvent);
     }
 
     public void loadHubEvent(HubEvent hubEvent) {
+        log.info("Start hub event save {}", hubEvent);
         kafkaProducer.send(hubEventMapper.mapToAvro(hubEvent), kafkaProperties.getTopic().getHub());
+        log.info("Success hub event save {}", hubEvent);
     }
 }
