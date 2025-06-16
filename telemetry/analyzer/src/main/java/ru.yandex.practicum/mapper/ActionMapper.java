6c7ed28357ed98d2c8e595d691fbcffef0f41109
Maria <peggy-sju@ya.ru>
2025-06-16 14:32:40 +0300

    Исправление ошибок 25

--- telemetry/analyzer/src/main/java/ru.yandex.practicum/mapper/ActionMapper.java
+++ telemetry/analyzer/src/main/java/ru.yandex.practicum/mapper/ActionMapper.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.yandex.practicum.grpc.telemetry.event.DeviceActionProto;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceActionAvro;
 import ru.yandex.practicum.model.Action;
@@ -8,5 +9,7 @@ import ru.yandex.practicum.model.Action;
 @Mapper(componentModel = "spring")
 public interface ActionMapper {
     DeviceActionProto mapToProto(Action action);
+
+    @Mapping(target = "sensor.id", source = "sensorId")
     Action mapFromAvro(DeviceActionAvro avro);
 }
