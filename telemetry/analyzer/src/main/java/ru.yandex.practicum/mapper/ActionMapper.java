2c84a8eef70b769fad1cf95baa08834952705a66
Maria <peggy-sju@ya.ru>
2025-06-16 18:02:38 +0300

    Исправление ошибок 46

--- telemetry/analyzer/src/main/java/ru.yandex.practicum/mapper/ActionMapper.java
+++ telemetry/analyzer/src/main/java/ru.yandex.practicum/mapper/ActionMapper.java
@@ -8,9 +8,11 @@ import ru.yandex.practicum.model.Action;
 
 @Mapper(componentModel = "spring")
 public interface ActionMapper {
-    @Mapping(target = "sensorId", source = "sensor_id")
+    @Mapping(target = "sensorId", source = "sensor.id")
     DeviceActionProto mapToProto(Action action);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "scenarios", ignore = true)
     @Mapping(target = "sensor.id", source = "sensorId")
     Action mapFromAvro(DeviceActionAvro avro);
 }
