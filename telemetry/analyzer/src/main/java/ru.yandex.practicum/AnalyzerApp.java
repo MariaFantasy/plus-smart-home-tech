8ac528c512a6293c755f46925f5d8b3b74b2e1db
Maria <peggy-sju@ya.ru>
2025-06-16 16:49:04 +0300

    Исправление ошибок 40

--- telemetry/analyzer/src/main/java/ru.yandex.practicum/AnalyzerApp.java
+++ telemetry/analyzer/src/main/java/ru.yandex.practicum/AnalyzerApp.java
@@ -3,24 +3,20 @@ package ru.yandex.practicum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
-import ru.yandex.practicum.grpc.telemetry.hubrouter.HubRouterControllerGrpc;
 import ru.yandex.practicum.service.hub.HubEventProcessor;
 import ru.yandex.practicum.service.snapshot.SnapshotProcessor;
 
 @Slf4j
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableConfigurationProperties(KafkaProperties.class)
 public class AnalyzerApp {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApp.class, args);
 
-        Environment env = context.getEnvironment();
-        log.info("GRPC: {}", env.getProperty("grpc.client.hub-router.address"));
-
-        final HubRouterControllerGrpc.HubRouterControllerBlockingStub grpcClient = context.getBean(HubRouterControllerGrpc.HubRouterControllerBlockingStub.class);
         final HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
         SnapshotProcessor snapshotProcessor = context.getBean(SnapshotProcessor.class);
 
