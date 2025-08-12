b82eef8eadab63c56fefbe1e36b883f4b284a7f9
Maria <peggy-sju@ya.ru>
2025-08-12 22:02:30 +0300

    Добавление Warehouse

--- commerce/warehouse/src/main/java/ru/yandex/practicum/controller/WarehouseController.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/controller/WarehouseController.java
@@ -1,4 +1,47 @@
 package ru.yandex.practicum.controller;
 
-public class WarehouseController {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.dto.*;
+import ru.yandex.practicum.feign.client.WarehouseClient;
+import ru.yandex.practicum.service.WarehouseService;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/api/v1/warehouse")
+@RequiredArgsConstructor
+public class WarehouseController implements WarehouseClient {
+    private final WarehouseService warehouseService;
+
+    @Override
+    public void putNewProduct(NewProductInWarehouseRequest request) {
+        log.info("Пришел PUT запрос с телом {}", request);
+        warehouseService.putNewProduct(request);
+        log.info("Отработан PUT запрос с телом {}", request);
+    }
+
+    @Override
+    public BookedProductsDto checkAvailable(ShoppingCartDto shoppingCartDto) {
+        log.info("Пришел POST запрос /check с телом {}", shoppingCartDto);
+        final BookedProductsDto bookedProductsDto = warehouseService.checkAvailable(shoppingCartDto);
+        log.info("Отправлен ответ POST запрос /check с телом {}", bookedProductsDto);
+        return bookedProductsDto;
+    }
+
+    @Override
+    public void addNewProduct(AddProductToWarehouseRequest request) {
+        log.info("Пришел POST запрос /add с телом {}", request);
+        warehouseService.addNewProduct(request);
+        log.info("Отработан POST запрос /add с телом {}", request);
+    }
+
+    @Override
+    public AddressDto getAddress() {
+        log.info("Пришел GET запрос /address");
+        final AddressDto addressDto = warehouseService.getAddress();
+        log.info("Отправлен ответ GET запрос /address с телом {}", addressDto);
+        return addressDto;
+    }
 }
--- commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/ProductDtoMapper.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/ProductDtoMapper.java
@@ -1,7 +1,16 @@
 package ru.yandex.practicum.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.yandex.practicum.dto.NewProductInWarehouseRequest;
+import ru.yandex.practicum.model.Product;
 
-@Mapper
-public class ProductMapper {
+@Mapper(componentModel = "spring")
+public interface ProductDtoMapper {
+    @Mapping(target = "id", source = "productId")
+    @Mapping(target = "width", source = "productDto.dimensionDto.width")
+    @Mapping(target = "height", source = "productDto.dimensionDto.height")
+    @Mapping(target = "depth", source = "productDto.dimensionDto.depth")
+    @Mapping(target = "quantity", ignore = true)
+    Product mapFromDto(NewProductInWarehouseRequest productDto);
 }
