726d44283c559b008da33366fc67eb209a64be9b
MariaFantasy <peggy-sju@ya.ru>
2025-08-13 16:34:32 +0300

    Update PageableDto.java

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/PageableDto.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/PageableDto.java
@@ -4,16 +4,17 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @lombok.Data
 @lombok.AllArgsConstructor
 public class PageableDto {
     @PositiveOrZero
-    private Integer page;
+    private Integer page = 0;
 
     @Positive
-    private Integer size;
+    private Integer size = 1;
 
-    private List<String> sort;
+    private List<String> sort = new ArrayList<>();
 }
